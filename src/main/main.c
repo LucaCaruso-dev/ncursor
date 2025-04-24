d5a16f707397a703bc843bb9d29833fbfed5f28d
Luca Caruso <luca+github@enricocaruso.net>
2025-04-24 22:44:56 +0200

    Feat: Read properties
    Read properties and insert info in Data scructure

--- src/main/main.c
+++ src/main/main.c
@@ -16,21 +16,24 @@
 
 #define MAX_STR_LEN 256
 
-int x, y;
-FILE *LanFile;
-
 struct Data
 {
-    char *label1;
-    char *label2;
-    char *label3;
-    char *label4;
-    char *label5;
-    char *label6;
-    char *email;
-    char *pwd;
+    char label1[MAX_STR_LEN];
+    char label2[MAX_STR_LEN];
+    char label3[MAX_STR_LEN];
+    char label4[MAX_STR_LEN];
+    char label5[MAX_STR_LEN];
+    char label6[MAX_STR_LEN];
+    char email[MAX_STR_LEN];
+    char pwd[MAX_STR_LEN];
 };
 typedef struct Data Data;
+
+int x, y;
+FILE *LanFile;
+int temp = 0;
+
+Data data;
 
 /*
  * for printing string in specified coordinates
@@ -42,6 +45,12 @@ typedef struct Data Data;
  * mvprintw(y, x, str, ...);       print
  * refresh();                       refresh the screen
  */
+
+void printContent(int x, int y, const char *str)
+{
+    mvprintw(y, x, "%s", str);
+    refresh();
+}
 
 bool readProperties(FILE *File, const char *Key, char *Value)
 {
@@ -61,14 +70,16 @@ bool readProperties(FILE *File, const char *Key, char *Value)
         KeyFound = strtok(Line, Token);
         if (strcmp(Line, Key) == 0)
         {
-            Value = strtok(NULL, Token);
+            // Value = strtok(NULL, Token);
+            strcpy(Value, strtok(NULL, Token));
+            // printContent(0, temp++, Value);
             return true; // Key found
         }
     }
     return false; // Key not found
 }
 
-bool setUpLan(Data *data)
+bool setUpLan()
 {
     /*
      * Open the file lan/%lan%.properties in read mode
@@ -114,31 +125,25 @@ bool setUpLan(Data *data)
      * data.pwd => userInput
      */
 
-    if (!readProperties(LanFile, "InsertEmail", data->label1))
+    /*char label1[MAX_STR_LEN] = "";
+    char label2[MAX_STR_LEN] = "";
+    char label3[MAX_STR_LEN] = "";
+    char label4[MAX_STR_LEN] = "";
+    char label5[MAX_STR_LEN] = "";
+    char label6[MAX_STR_LEN] = "";*/
+
+    if (!readProperties(LanFile, "InsertEmail", data.label1) ||
+        !readProperties(LanFile, "Email", data.label2) ||
+        !readProperties(LanFile, "InsertPassword", data.label3) ||
+        !readProperties(LanFile, "Password", data.label4) ||
+        !readProperties(LanFile, "PasswordConfirmation", data.label5) ||
+        !readProperties(LanFile, "Login", data.label6))
     {
-        return false;
-    }
-    if (!readProperties(LanFile, "Email", data->label2))
-    {
-        return false;
-    }
-    if (!readProperties(LanFile, "InsertPassword", data->label3))
-    {
-        return false;
-    }
-    if (!readProperties(LanFile, "Password", data->label4))
-    {
-        return false;
-    }
-    if (!readProperties(LanFile, "PasswordConfirmation", data->label5))
-    {
-        return false;
-    }
-    if (!readProperties(LanFile, "Login", data->label6))
-    {
+        fclose(LanFile);
         return false;
     }
 
+    fclose(LanFile);
     return true;
 }
 
@@ -152,8 +157,16 @@ start:
     noecho();              // Disabilita l'echo dei caratteri
     nodelay(stdscr, TRUE); // Rende getch() non bloccante
 
-    Data data;
-    if (!setUpLan(&data))
+    data.label1[0] = '\0';
+    data.label2[0] = '\0';
+    data.label3[0] = '\0';
+    data.label4[0] = '\0';
+    data.label5[0] = '\0';
+    data.label6[0] = '\0';
+    data.email[0] = '\0';
+    data.pwd[0] = '\0';
+
+    if (!setUpLan())
     {
         endwin();
         return 1;
@@ -162,16 +175,40 @@ start:
     clear();
 
     bool quit = false;
+    int select = 0;
+    int max_select = 3;
+
+    printContent(2, 0, data.label1);
+    printContent(2, 1, data.label2);
+    printContent(2, 2, data.label3);
+    printContent(2, 3, data.label4);
+    printContent(2, 4, data.label5);
+    printContent(2, 5, data.label6);
+    printContent(2, 6, "Press CTR+C to quit");
 
     while (!quit)
     {
+
         int ch = getch();
+        switch (select)
+        {
+        case 0:
+            
+            break;
+        }
         switch (ch)
         {
-        case 'q':
-            quit = true;
+        case KEY_DOWN:
+            if (select < max_select)
+                select++;
+            else
+                select = 0;
             break;
-        default:
+        case KEY_UP:
+            if (select > 0)
+                select--;
+            else
+                select = max_select;
             break;
         }
     }
