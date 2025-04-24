6c91bb2c3388e25fae0c5979d3e9d1c9f5ddc287
Luca Caruso <luca+github@enricocaruso.net>
2025-04-24 21:40:13 +0200

    Fead: add function for read properties file

--- src/main/main.c
+++ src/main/main.c
@@ -3,11 +3,18 @@
  * Run with: ./ncurses
  */
 
+/*
+ * Press 'q' to quit
+ */
+
 #include <curses.h>
 #include <stdbool.h>
 #include <stdarg.h>
 #include <stdio.h>
 #include <stdlib.h>
+#include <string.h>
+
+#define MAX_STR_LEN 256
 
 int x, y;
 FILE *LanFile;
@@ -16,6 +23,12 @@ struct Data
 {
     char *label1;
     char *label2;
+    char *label3;
+    char *label4;
+    char *label5;
+    char *label6;
+    char *email;
+    char *pwd;
 };
 typedef struct Data Data;
 
@@ -26,11 +39,36 @@ typedef struct Data Data;
  * string: str (with format specifiers)
  * ...: variable arguments
  *
- * mvprintw(x, y, str, ...);       print
+ * mvprintw(y, x, str, ...);       print
  * refresh();                       refresh the screen
  */
 
-bool setUpLan()
+bool readProperties(FILE *File, const char *Key, char *Value)
+{
+    /*
+     * Read the file line by line
+     * If the line contains the key, extract the value and store it in data
+     * Return true if the key is found, false not found
+     * The file format is: %key% = %value%
+     *
+     * Please don't use char '=' in value
+     */
+    char Line[MAX_STR_LEN];
+    const char *Token = "=";
+    char *KeyFound;
+    while (fgets(Line, MAX_STR_LEN, File))
+    {
+        KeyFound = strtok(Line, Token);
+        if (strcmp(Line, Key) == 0)
+        {
+            Value = strtok(NULL, Token);
+            return true; // Key found
+        }
+    }
+    return false; // Key not found
+}
+
+bool setUpLan(Data *data)
 {
     /*
      * Open the file lan/%lan%.properties in read mode
@@ -39,21 +77,68 @@ bool setUpLan()
     char *lan = getenv("LANG");
     char filepath[100];
 
+    int line = 0;
+
     snprintf(filepath, sizeof(filepath), "lan/%.2s.properties", lan);
-    mvprintw(0, 0, "Opening file %s", filepath);
+    mvprintw(line++, 0, "Opening file %s", filepath);
     refresh();
 
     LanFile = fopen(filepath, "r");
     if (LanFile == NULL)
     {
+        mvprintw(line++, 0, "Error: File %s not found", filepath);
         LanFile = fopen("lan/it.properties", "r");
+        mvprintw(line++, 0, "Opening file lan/it.properties");
+        refresh();
         if (LanFile == NULL)
         {
-            mvprintw(0, 0, "Error: File %s not found", filepath);
+            mvprintw(line++, 0, "Error: File %s not found", filepath);
             refresh();
+            napms(1000); // Pausa di 1 secondo
             return false;
         }
     }
+
+    napms(1000); // Pausa di 1 secondo
+
+    /*
+     * Data data;
+     * data.label1 = %InsertEmail%;
+     * data.label2 = %Email%;
+     * data.label3 = %InsertPassword%;
+     * data.label4 = %Password%;
+     * data.label5 = %PasswordConfirmation%;
+     * data.label6 = %Login%;
+     *
+     * data.email => userInput
+     * data.pwd => userInput
+     */
+
+    if (!readProperties(LanFile, "InsertEmail", data->label1))
+    {
+        return false;
+    }
+    if (!readProperties(LanFile, "Email", data->label2))
+    {
+        return false;
+    }
+    if (!readProperties(LanFile, "InsertPassword", data->label3))
+    {
+        return false;
+    }
+    if (!readProperties(LanFile, "Password", data->label4))
+    {
+        return false;
+    }
+    if (!readProperties(LanFile, "PasswordConfirmation", data->label5))
+    {
+        return false;
+    }
+    if (!readProperties(LanFile, "Login", data->label6))
+    {
+        return false;
+    }
+
     return true;
 }
 
@@ -67,8 +152,8 @@ start:
     noecho();              // Disabilita l'echo dei caratteri
     nodelay(stdscr, TRUE); // Rende getch() non bloccante
 
-    napms(1000); // Pausa di 1 secondo
-    if (!setUpLan())
+    Data data;
+    if (!setUpLan(&data))
     {
         endwin();
         return 1;
@@ -76,19 +161,18 @@ start:
 
     clear();
 
-    Data data;
-
     bool quit = false;
 
     while (!quit)
     {
         int ch = getch();
-        switch (ch) {
-            case 'q':
-                quit = true;
-                break;
-            default:
-                break;
+        switch (ch)
+        {
+        case 'q':
+            quit = true;
+            break;
+        default:
+            break;
         }
     }
 
