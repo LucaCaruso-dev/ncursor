27a30e6a265fd3f2031fe6afd8595310cbfdf216
Luca Caruso <luca+github@enricocaruso.net>
2025-05-20 09:59:34 +0200

    Update contatto.h

--- src/main/contatto.h
+++ src/main/contatto.h
@@ -1,16 +1,17 @@
-#ifndef CONTATTO_H
+#ifndef CONTATTO_H // Prevenzione della doppia inclusione
 #define CONTATTO_H
 
-#include <curses.h>
-#include "lan.h"
+#include <curses.h> // Inclusione per la gestione dell'interfaccia utente in modalità testo
+#include "lan.h" // Inclusione del file header per la gestione delle lingue
 
-#define SelColor 10
-#define NotSelColor 1
+#define SelColor 10 // Colore per la selezione
+#define NotSelColor 1 // Colore per la non selezione
 
+// Dichiarazione delle funzioni per la gestione dei contatti
 void drawContent(int select, char*nome, char*cognome, char*eta, char*numero, char*citta);
 void searchButton();
 void addButton();
 void drawChangeModeButton();
 void drawExitButton();
 
-#endif
+#endif // Fine della protezione
